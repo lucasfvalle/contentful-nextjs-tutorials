d16d260b5e8490ec4bbabcbbc5d33f2f3d1c0167
Lucas Valle <lucasfvalle@gmail.com>
2024-09-12 15:32:20 -0300

    testando

--- components/data/details/ProductDetails.jsx
+++ components/data/details/ProductDetails.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ProductCard from '../cards/ProductCard'
+import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
 
 const ProductDetails = ({product}) =>{
     console.log('abaixo:')
     console.log(product)
     return(
-
+        <div>
         <div>{product.fields.title}</div>
+        <div>{documentToReactComponents(product.fields.description)}</div>
+        </div>
+
     )
 }
 export default ProductDetails
